5c5e0a4e78ff0f55a072bba190e10f69bef93033
Siva Movva <siva.s.movva@intel.com>
2024-10-25 11:28:59 -0700

    app loading speed is in a much better state now. main thing that helped is lazy imports - import libraries and functions only right before they are needed. Reduced initial web app load time from 30 seconds to less than 1 second!

--- app.py
+++ app.py
@@ -1,45 +1,38 @@
 # %%
 import time  # noqa
-start_time = time.time()  # noqa
-
-
-import pandas as pd  # noqa
 import streamlit as st  # noqa
 
-
-from tennis_eda_dataprocessing import (
-
-    get_player_match_subset_against_tour
-
-)  # noqa
-end_time = time.time()  # noqa
 # %%
 # Use the full width of the page
 st.set_page_config(layout="wide")
 
-st.write(
-    f"Time to import and setup (new): {end_time - start_time:.2f} seconds")
 
 # %%
 # Title of the app
 st.title('Pro Tennis -  How do the most successful players win?')
 
-st.write(
-    "In this data app, we will analyze the players who have played at least 100 ATP matches since 1990 (a tiny fraction of the total number of players who played professional tennis) and see the factors that determined whether they won or lost a match")
 
-start_time = time.time()
-try:
-    # Read the player_subset.parquet file
-    df_player_subset = pd.read_parquet('player_subset.parquet')
-except FileNotFoundError:
-    st.error("The file 'player_subset.parquet' was not found. Please ensure the file is in the correct location.")
-    st.stop()
-except Exception as e:
-    st.error(f"An error occurred while reading the file: {e}")
-    st.stop()
-end_time = time.time()
-st.write(
-    f"Time to read player_subset file: {end_time - start_time:.2f} seconds")
+# Text to display with typing effect
+intro_text = "In this data app, we will analyze the players who have played at least 150 ATP matches since 1990 (about 400 out of the >3000 professionals) and see the factors that determined whether they won or lost a match"
+st.write(intro_text)
+
+
+with st.spinner('Getting player list who have played at least 150 matches...'):
+    try:
+        global pd, get_player_match_subset_against_tour, df_player_subset  # noqa
+        import pandas as pd  # noqa
+
+        # Read the player_subset.parquet file
+        df_player_subset = pd.read_parquet('player_subset.parquet')
+
+    except FileNotFoundError:
+        st.error(
+            "The file 'player_subset.parquet' was not found. Please ensure the file is in the correct location.")
+        st.stop()
+    except Exception as e:
+        st.error(f"An error occurred while reading the file: {e}")
+        st.stop()
+
 
 # Get the list of players from the first column of the dataframe
 players = df_player_subset.iloc[:, 0].unique().tolist()
@@ -57,4 +50,15 @@ filtered_players = [
 user_selected_player = st.selectbox(
     'Or Select Player directly here (warning: Long list !)', filtered_players, index=0 if filtered_players else -1)
 
-df_concat_subset = get_player_match_subset_against_tour(user_selected_player)
+with st.spinner('Getting player match subset...'):
+    try:
+        from tennis_eda_dataprocessing import (
+            get_player_match_subset_against_tour
+            )   # noqa
+        df_concat_subset = get_player_match_subset_against_tour(
+            user_selected_player)
+
+    except Exception as e:
+        st.error(
+            f"An error occurred while getting the player match subset: {e}")
+        st.stop()
